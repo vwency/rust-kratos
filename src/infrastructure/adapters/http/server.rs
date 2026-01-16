cdfb5a94739a05a9417780e09b6a5ba2adbe473d
vwency <veniaminshp@gmail.com>
2026-01-17 01:08:17 +0600

    chore: prometheus

--- src/infrastructure/adapters/http/server.rs
+++ src/infrastructure/adapters/http/server.rs
@@ -4,17 +4,23 @@ use crate::infrastructure::adapters::graphql::handlers::{graphql_handler, graphq
 use crate::infrastructure::adapters::graphql::schema::AppSchema;
 use actix_cors::Cors;
 use actix_web::{App, HttpServer, web};
+use actix_web_prometheus::PrometheusMetricsBuilder;
 use std::sync::Arc;
 use tracing::info;
 use tracing_actix_web::TracingLogger;
 
 pub async fn start(schema: Arc<AppSchema>, config: ServerConfig) -> std::io::Result<()> {
     let bind_address = format!("{}:{}", config.host, config.port);
-
     info!("Booting HTTP server at http://{}", bind_address);
+
+    let prometheus = PrometheusMetricsBuilder::new("api")
+        .endpoint("/metrics")
+        .build()
+        .unwrap();
 
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(prometheus.clone())
             .wrap(TracingLogger::default())
             .wrap(
                 Cors::default()
@@ -37,6 +43,7 @@ pub async fn start(schema: Arc<AppSchema>, config: ServerConfig) -> std::io::Res
         bind_address
     );
     info!("🚀 GraphQL Playground: http://{}/graphql", bind_address);
+    info!("📊 Prometheus Metrics: http://{}/metrics", bind_address);
 
     server.run().await
 }
